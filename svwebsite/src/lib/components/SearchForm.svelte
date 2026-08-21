b0a340a7c876d2c46851ba8c3aaaa91bd9c54a64
FabRondeau <contact@informaid.fr>
2026-08-21 12:49:42 +0200

    Change website name.

---
+++ svwebsite/src/lib/components/SearchForm.svelte
@@ -0,0 +1,9 @@
+<form method="POST">
+	<label>
+		<input class="px-5 py-2 w-1/1"
+      placeholder="Search"
+			name="description"
+			autocomplete="off"
+		/>
+	</label>
+</form>
---
+++ svwebsite/src/routes/contact/+page.svelte
@@ -0,0 +1,3 @@
+<!-- Tailwindcss classes. -->
+
+<h1 class="bg-white p-4 rounded-lg shadow-md">Contact</h1>
